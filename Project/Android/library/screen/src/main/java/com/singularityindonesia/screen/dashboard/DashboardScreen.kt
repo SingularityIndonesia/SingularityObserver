b7333a95d821ac5bf23906eb0dff129de6591dd4
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-03-30 03:55:40 +0700

    feat(dashboard): add DashboardScreen.

--- Project/Android/library/screen/src/main/java/com/singularityindonesia/screen/dashboard/DashboardScreen.kt
+++ Project/Android/library/screen/src/main/java/com/singularityindonesia/screen/dashboard/DashboardScreen.kt
@@ -1,2 +1,14 @@
 package com.singularityindonesia.screen.dashboard
 
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DashboardScreen(
+    onTakePictureClick: () -> Unit
+) {
+    Button(onClick = onTakePictureClick) {
+        Text(text = "Take Picture")
+    }
+}
